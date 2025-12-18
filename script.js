ec6d43a4d10e5a72e6d1827df9d8fdc3b694fe30
sandrojoe1103-del <sandrojoe1103@gmail.com>
2025-12-18 11:28:59 +0530

    Add calculator files

---
+++ script.js
@@ -0,0 +1,21 @@
+let display = document.getElementById('display');
+
+function appendToDisplay(value) {
+    display.textContent += value;
+}
+
+function clearDisplay() {
+    display.textContent = '';
+}
+
+function deleteLast() {
+    display.textContent = display.textContent.slice(0, -1);
+}
+
+function calculateResult() {
+    try {
+        display.textContent = eval(display.textContent);
+    } catch (error) {
+        display.textContent = 'Error';
+    }
+}
